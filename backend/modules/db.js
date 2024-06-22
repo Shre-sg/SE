6c7bb958f83a2fe20ead48470dd750eab8da8036
Shre-sg <shreyasg.is21@gmail.com>
2024-06-22 23:20:42 +0530

    db update

--- backend/modules/db.js
+++ backend/modules/db.js
@@ -1,11 +1,29 @@
 const mysql = require('mysql2');
 
+//shreyas db
 const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: 'Lingaiah@60',
     database: 'team_database',
 });
+
+////spoorthi db
+// const db = mysql.createConnection({
+//   host: 'localhost',
+//   user: 'root',
+//   password: 'Lingaiah@60',
+//   database: 'team_database',
+// });
+
+////sanjana db
+// const db = mysql.createConnection({
+//   host: 'localhost',
+//   user: 'root',
+//   password: 'Lingaiah@60',
+//   database: 'team_database',
+// });
+
 
 db.connect((err) => {
     if (err) {
