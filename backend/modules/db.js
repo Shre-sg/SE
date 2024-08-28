aa07cf1919570b41ac3b6dcb9d1111065aa2c8f9
Shre-sg <shreyasg.is21@gmail.com>
2024-08-28 18:10:54 +0530

    done all

--- backend/modules/db.js
+++ backend/modules/db.js
@@ -1,20 +1,21 @@
 const mysql = require('mysql2');
 
 //shreyas db
-const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'Lingaiah@60',
-    database: 'PLACEMENT', //RV_COLLEGE_PLACEMENT
-});
+// const db = mysql.createConnection({
+//     host: 'localhost',
+//     user: 'root',
+//     password: 'Lingaiah@60',
+//     database: 'PLACEMENT', //RV_COLLEGE_PLACEMENT
+// });
 
 ////spoorthi db
-// const db = mysql.createConnection({
-//   host: 'localhost',
-//   user: 'root',
-//   password: 'Lingaiah@60',
-//   database: 'team_database',
-// });
+const db = mysql.createConnection({
+  host: 'mysql-2c83732b-shreyasg-5f50.h.aivencloud.com',
+  user: 'avnadmin',
+  password: 'AVNS_QDqZodIseE2sz48Ax1H',
+  database: 'PLACEMENT',
+  port : '21146',
+});
 
 ////sanjana db
 // const db = mysql.createConnection({
