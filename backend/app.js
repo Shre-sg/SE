1498c601435d04630991e9a218d17594103d5a24
Shre-sg <shreyasg.is21@gmail.com>
2024-08-30 18:41:37 +0530

    supp

--- backend/app.js
+++ backend/app.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const session = require('express-session');
 require('dotenv').config();
-console.log('AIVEN_SERVICE_PASSWORD:', process.env.AIVEN_SERVICE_PASSWORD); // Debugging line
+
 
 const fileUpload = require('express-fileupload');
 const xlsx = require('xlsx');
