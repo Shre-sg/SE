e84cdf4f098e9fd9d88a93b103ad0ed154609ac7
Shre-sg <shreyasg.is21@gmail.com>
2024-08-30 19:37:21 +0530

    tee

--- backend/modules/db.js
+++ backend/modules/db.js
@@ -2,12 +2,12 @@
 const mysql = require('mysql2');
 
 //shreyas db
-const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'Lingaiah@60',
-    database: 'PLACEMENT', //RV_COLLEGE_PLACEMENT
-});
+// const db = mysql.createConnection({
+//     host: 'localhost',
+//     user: 'root',
+//     password: 'Lingaiah@60',
+//     database: 'PLACEMENT', //RV_COLLEGE_PLACEMENT
+// });
 
 // const db = mysql.createConnection({
 //   host: 'mysql-2c83732b-shreyasg-5f50.h.aivencloud.com',
